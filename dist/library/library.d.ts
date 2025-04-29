bc4b25b92b344b1f00eb9478ae9f200a646851ac
QANSIGLIERE <qansigliere@gmail.com>
2025-04-29 20:31:28 +0300

    JS-281

---
+++ dist/library/library.d.ts
@@ -0,0 +1,3 @@
+export function parseJSONReporterFolderAndGenerateCSVFile(pathToFolder: any): void;
+export function parseJSONReporterAndSyncResultsToTestrail(pathToFolderWithJSONResults: any, url: any, username: any, apiKey: any, projectID: any, suiteID: any, testRunID: any, testRunName: any): Promise<void>;
+//# sourceMappingURL=library.d.ts.map
